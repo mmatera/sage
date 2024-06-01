9382da964db4a29af8af86acf6f5255bfd6ce768
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-06-01 16:37:22 -0700

    fixups

--- src/sage/rings/rational_field.py
+++ src/sage/rings/rational_field.py
@@ -1700,7 +1700,7 @@ def is_RationalField(x) -> bool:
 
     EXAMPLES::
 
-        sage: from sage.rings.rational_field import RationalField as is_RF
+        sage: from sage.rings.rational_field import is_RationalField as is_RF
         sage: is_RF(QQ)
         True
         sage: is_RF(ZZ)
